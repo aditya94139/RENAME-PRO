bbc53d4a826cd41c880ef164f034636c98c5ce60
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-04-16 09:51:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ bot = Client(
 
            "Renamer",
 
-           bot_token=BOT_TOKEN,
+           bot_token=7055338137:AAFgB8eDvyEo4JZQbgtmGoz4P8lngDgRHsc,
 
-           api_id=API_ID,
+           api_id=23163380,
 
-           api_hash=API_HASH,
+           api_hash=2dca155e786c7db2d295e5b4ab10783b,
 
            plugins=dict(root='plugins'))
            
